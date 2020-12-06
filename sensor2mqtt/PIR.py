e3aa29790d7a6c07f1e125c8d866aa16f5ef5604
David Greaves <david@dgreaves.com>
2020-12-06 19:05:00 +0000

    It's a topic, not a channel
    Signed-off-by: David Greaves <david@dgreaves.com>

--- sensor2mqtt/PIR.py
+++ sensor2mqtt/PIR.py
@@ -13,18 +13,20 @@ class PIR:
         self.mqtt = mqtt
         self.quiet = quiet
         host = socket.gethostname()
-        self.m_chan = f"sensor/pir/{host}/{pin}"
-        logger.info(f"Setting PIR on pin {pin}: {self.m_chan}")
-        self.pir = MotionSensor(pin=pin, pull_up=True)
+        self.m_topic = f"sensor/pir/{host}/{pin}"
+        logger.info(f"Setting PIR on pin {pin}: {self.m_topic}")
+        self.pir = MotionSensor(pin=pin)
         self.pir.when_motion = self.motion
         self.pir.when_no_motion = self.no_motion
         self.loop = asyncio.get_running_loop()
         self.no_motion()
 
     def motion(self):
+        logger.debug(f"motion on pin {self.m_topic}")
         self.loop.call_soon_threadsafe(self.mqtt.publish,
-                                       self.m_chan, True)
+                                       self.m_topic, True)
 
     def no_motion(self):
+        logger.debug(f"no motion on pin {self.m_topic}")
         self.loop.call_soon_threadsafe(self.mqtt.publish,
-                                       self.m_chan, False)
+                                       self.m_topic, False)
