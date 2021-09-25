d808f9a11cec508276ccdee14d57ff718927a081
David Greaves <david@dgreaves.com>
2021-09-25 14:35:48 +0100

    Make pip installable
    Signed-off-by: David Greaves <david@dgreaves.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import setuptools
 
-setup(
-    name='sensor2mqtt',
-    version='0.1dev',
-    packages=['sensor2mqtt',],
-    scripts=['bin/broadcast_temps.py'],
-    license='GPLv3',
-    long_description="""
-    Sends MQTT messagesa about the 1-wire temperature probes on a system
-    """
-    install_requires=[
-        "toml",
-        "gmqtt",
-        "gpiozero",
-        "RPi.GPIO",
-    ],
-)
+# setup() now reads setup.cfg
+if __name__ == "__main__":
+    setuptools.setup()
