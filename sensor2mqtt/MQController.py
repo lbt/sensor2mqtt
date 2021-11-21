deb7ff333e7b3c60316b9372ad278ebd09190508
David Greaves <david@dgreaves.com>
2021-11-21 19:40:51 +0000

    Use the generic MQController class which may be broken out one day
    Signed-off-by: David Greaves <david@dgreaves.com>

---
+++ sensor2mqtt/MQController.py
@@ -0,0 +1,124 @@
+import asyncio
+import inspect
+import logging
+import os
+import signal
+import socket
+
+from gmqtt import Client as MQTTClient
+from gmqtt.mqtt.constants import MQTTv311
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MQController:
+    def __init__(self, config):
+        self._loop = asyncio.get_event_loop()
+        self.subscriptions = []
+        self.handlers = []
+        self.config = config
+        self.host = socket.gethostname()
+        self.cleanup_callbacks = set()
+        self.stop_event = asyncio.Event()
+        self.mqtt = None
+
+    async def connect(self):
+        self.mqtt = MQTTClient(f"{socket.gethostname()}.{os.getpid()}")
+        self.mqtt.set_auth_credentials(username=self.config["username"],
+                                       password=self.config["password"])
+
+        self.mqtt.on_connect = self.on_connect
+        self.mqtt.on_message = self.on_message
+        self.mqtt.on_disconnect = self.on_disconnect
+
+        self._loop.add_signal_handler(signal.SIGINT, self.ask_exit)
+        self._loop.add_signal_handler(signal.SIGTERM, self.ask_exit)
+        self._loop.set_exception_handler(self.handle_exception)
+
+        mqtt_host = self.config["mqtt_host"]
+        mqtt_version = MQTTv311
+
+        # Connect to the broker
+        while not self.mqtt.is_connected:
+            try:
+                await self.mqtt.connect(mqtt_host, version=mqtt_version)
+            except Exception as e:
+                LOGGER.warn(f"Error trying to connect: {e}. Retrying.")
+                await asyncio.sleep(1)
+
+    async def finish(self):
+        # This will wait until the client is signalled
+        LOGGER.debug(f"Waiting for stop event")
+        await self.stop_event.wait()
+        LOGGER.debug(f"Stop received, cleaning up")
+        for cb in self.cleanup_callbacks:
+            res = cb()
+            if inspect.isawaitable(res):
+                await res
+
+        await self.mqtt.disconnect()  # Disconnect after any last messages sent
+        LOGGER.debug(f"client disconnected")
+
+    def add_handler(self, handler):
+        '''A handler takes a topic/payload and returns true if it handles the
+        topic
+        '''
+        if handler not in self.handlers:
+            self.handlers.append(handler)
+
+    def add_cleanup_callback(self, handler):
+        if handler not in self.cleanup_callbacks:
+            self.cleanup_callbacks.add(handler)
+
+    async def on_message(self, _client, topic, payload, _qos, _properties):
+        handled = False
+        tasks = list()
+
+        for h in self.handlers:
+            LOGGER.debug(f"handler for {topic} : {h}")
+            res = h(topic, payload)
+            if inspect.isawaitable(res):
+                # it's a async callback
+                tasks.append(res)
+            else:
+                handled |= res
+
+        res_list = await asyncio.gather(*tasks)
+        for res in res_list:
+            handled |= res
+        if not handled:
+            LOGGER.warning(f"FYI: Unhandled message {topic} = {payload}")
+
+    def subscribe(self, topic):
+        """Subscribes to an MQTT topic (passed directly to MQTT)"""
+        if topic not in self.subscriptions:
+            self.subscriptions.append(topic)
+            if self.mqtt:
+                LOGGER.debug(f"Subscribing to {topic}")
+                self.mqtt.subscribe(topic)
+
+    def on_connect(self, _client, _flags, _rc, _properties):
+        for s in self.subscriptions:
+            LOGGER.debug(f"Re-subscribing to {s}")
+            self.mqtt.subscribe(s)
+        LOGGER.debug('Connected and subscribed')
+
+    def on_disconnect(self, _client, _packet, _exc=None):
+        LOGGER.debug('Disconnected')
+
+    def publish(self, topic, payload, retain=True):
+        """Publish :param payload: to :param topic:"""
+        LOGGER.debug(f"Publishing {topic} = {payload}")
+        self.mqtt.publish(topic, payload, qos=2, retain=retain)
+
+    def ask_exit(self):
+        """Handle outstanding messages and cleanly disconnect"""
+        LOGGER.warning(f"{self} received signal asking to exit")
+        self.stop_event.set()
+
+    def handle_exception(loop, context):
+        # context["message"] will always be there; but
+        # context["exception"] may not
+        msg = context.get("exception", context["message"])
+        LOGGER.error(f"Caught exception: {msg}", exc_info=True)
--- sensor2mqtt/__init__.py
+++ sensor2mqtt/__init__.py
@@ -1,3 +1,4 @@
+from .MQController import MQController
 from .SensorController import SensorController
 from .DS18B20s import DS18B20s
 from .PIR import PIR
